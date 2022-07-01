64f2492a4702a94e172c7f9046fc687f06ca703e
piztanza <m.salmanalhafizh@gmail.com>
2022-07-01 07:27:33 +0700

    Edit client ~~ package.json App.js Action-posts.js Component-Posts.js index.js Reducers-posts.js ~~ server ~~  index.js ~~

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb://piztanza:yelyahwilliams@cluster0-shard-00-00.hee9w.mongodb.net:27017,cluster0-shard-00-01.hee9w.mongodb.net:27017,cluster0-shard-00-02.hee9w.mongodb.net:27017/?ssl=true&replicaSet=atlas-2v0fs5-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.PORT || 5000;
